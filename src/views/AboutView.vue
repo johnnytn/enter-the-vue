547b059213ef05b5075da55a759a0838b768d140
johnnytn <johnnytn@gmail.com>
2022-09-16 18:02:56 -0300

    feat: add composition api to aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ title }} {{ count }}</h1>
+    <div>
+      <span>{{ description }}</span>
+
+      <button @click="increment">Increase!</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue"
+
+const title = "This is an about page"
+const description = "loreal ipssum"
+// reactive state
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
 
 <style>
 @media (min-width: 1024px) {
